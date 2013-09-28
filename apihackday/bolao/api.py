3b78aaf89abb17df92b2364115a6c22f223a64ff
Dalton Barreto <daltonmatos@gmail.com>
2013-09-28 17:54:48 -0300

    Configuring tastypie to use django auth*

--- apihackday/bolao/api.py
+++ apihackday/bolao/api.py
@@ -1,5 +1,6 @@
 #encoding: utf-8
-from tastypie.authorization import Authorization
+from tastypie.authorization import DjangoAuthorization
+from tastypie.authentication import SessionAuthentication
 from tastypie.resources import ModelResource
 from models import Apostador
 
@@ -8,4 +9,5 @@ class ApostadorResource(ModelResource):
     class Meta:
         queryset = Apostador.objects.all()
         resource_name = 'apostador'
-        authorization = Authorization()
+        authorization = DjangoAuthorization()
+        authentication = SessionAuthentication()
