14e6cb8574d109cfc992aeeea2d37b5df5fe6e6a
Dalton Barreto <daltonmatos@gmail.com>
2013-09-28 18:23:34 -0300

    Adicionando admin para os models

--- apihackday/bolao/admin.py
+++ apihackday/bolao/admin.py
@@ -1,10 +1,21 @@
 #encoding: utf-8
 
 from django.contrib import admin
-from models import Apostador
+from models import Apostador, Bolao, Aposta
 
 
 class ApostadorAdmin(admin.ModelAdmin):
     pass
 
+
+class BolaoAdmin(admin.ModelAdmin):
+    pass
+
+
+class ApostaAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Apostador, ApostadorAdmin)
+admin.site.register(Bolao, BolaoAdmin)
+admin.site.register(Aposta, ApostaAdmin)
