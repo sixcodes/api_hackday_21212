00bd8d7197dab10946d76739f6d8442d67022cd5
Dalton Barreto <daltonmatos@gmail.com>
2013-09-28 14:24:32 -0300

    Instalando v1 da API

--- apihackday/urls.py
+++ apihackday/urls.py
@@ -1,8 +1,13 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
+from tastypie.api import Api
+from bolao.api import ApostadorResource
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+
+v1_api = Api(api_name='v1')
+v1_api.register(ApostadorResource())
 
 urlpatterns = patterns('',
 
@@ -10,6 +15,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'apihackday.views.home', name='home'),
     # url(r'^apihackday/', include('apihackday.foo.urls')),
     url(r'', include('social_auth.urls')),
+    url(r'^api/', include(v1_api.urls)),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
