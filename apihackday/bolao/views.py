14b71bfaeebfb07d146b5eaa17f604020b14069d
Bruno Melo <brunousml@gmail.com>
2013-09-29 02:10:12 -0300

    adicionando js ao template

--- apihackday/bolao/views.py
+++ apihackday/bolao/views.py
@@ -17,7 +17,7 @@ def home(r):
     return render_to_response('home.html', context_instance=RequestContext(r))
 
 def listarBolao(r):
-    return render_to_response('listar-bolao.html', context_instance=RequestContext(r))
+    return render_to_response('listar-boloes.html', context_instance=RequestContext(r))
 
 def criarBolao(r):
     return render_to_response('form-criar-bolao.html', context_instance=RequestContext(r))
--- apihackday/static/js/api.js
+++ apihackday/static/js/api.js
@@ -1,9 +1,49 @@
 
 $(document).ready(function(e){
+    function todosBoloes(){
+        boloes = {
+            "Bolao":
+            {
+                "titulo": "Bolão HACKDAY",
+                "time_1": "Brasil",
+                "time_2": "Argentina",
+                "resultado_time_1": 3,
+                "resultado_time_2": 1,
+                "admin": "Bruno Melo",
+                "encerrado": 1
+            },
 
+            "Bolao":
+            {
+                "titulo": "Imagina na copa",
+                "time_1": "Brasil",
+                "time_2": "Uruguai",
+                "resultado_time_1": 0,
+                "resultado_time_2": 0,
+                "admin": "JJ",
+                "encerrado": 0
+            },
+
+            "Bolao":
+            {
+                "titulo": "Campeonato Carioca",
+                "time_1": "Flamengo",
+                "time_2": "Fluminence",
+                "resultado_time_1": 2,
+                "resultado_time_2": 2,
+                "admin": "Bruno Melo",
+                "encerrado": 1
+            }
+        };
+
+        var template = _.template($("#listar-boloes").html());
+        $('#panel-group').html(template({'bolao': boloes}));
+
+    }
 });
 
 listarBoloes = function(data){
+    console.log(data);
     var template = _.template($("#bolao-template").html());
     $('#bolao-render').html(template({'bolao': data}));
 
